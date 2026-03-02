79a1a0bc5cf0fdecafc4a704184dafa2a990ec00
Sameer <sameerjha236@gmail.com>
2026-03-03 02:08:58 +0530

    feat: add role-based authorization middleware for delete route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,18 @@
 import express from "express";
 import { AuthController } from "../controllers/auth.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { authorize } from "../middlewares/role.middleware.js";
 
 const router = express.Router();
 
 router.post("/login", AuthController.login);
 router.post("/signup", AuthController.signup);
-router.delete("/delete", authMiddleware, AuthController.delete);
+router.delete(
+  "/delete",
+  authMiddleware,
+  authorize("USER", "Admin"),
+  AuthController.delete,
+);
 router.get("/logout", authMiddleware, AuthController.logout);
+
 export default router;
